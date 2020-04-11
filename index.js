6b0ed96bb73fcf4c8fce8963942a262b86238b05
Dan Putt <dan.putt@globalpay.com>
2020-04-10 20:21:46 -0400

    Forgot a comma...again

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ const postToTeams = (body, res) => {
     const options = {
         hostname: 'outlook.office.com',
         port: 443,
-        path: '/webhook/3f42eefe-009f-4df6-8488-391f40011ee1@6c637512-c417-4e78-9d62-b61258e4b619/IncomingWebhook/89e5b17135ca42318a9a3f35c8308f66/2463e5cf-4111-453d-8f7d-099a35bb61dd'
+        path: '/webhook/3f42eefe-009f-4df6-8488-391f40011ee1@6c637512-c417-4e78-9d62-b61258e4b619/IncomingWebhook/89e5b17135ca42318a9a3f35c8308f66/2463e5cf-4111-453d-8f7d-099a35bb61dd',
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
